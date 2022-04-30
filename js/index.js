2a8a82e8210234469bd2061624360f4eada80be0
MrTron <seidelw@outlook.com>
2022-04-29 21:23:13 -0400

    Color Change and Trigger Works!

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-
+//Title Animation
+const titleTag = document.getElementById("titleTag");
+setTimeout(() => {
+    titleTag.classList.add('color-change'); 
+    titleTag.classList.remove('fadeIn'); 
+}, 2000);
+//Triggers
+const observer = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+      // If the element is visible
+      if (entry.isIntersecting) {
+        // Add the animation class
+        entry.target.classList.add('about-animation');
+      }
+    });
+  });
+  
+  observer.observe(document.querySelector('.about-header'));
