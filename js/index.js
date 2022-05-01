6cd1eb91f9d7fad11b6acdd53c78081b428b3003
MrTron <seidelw@outlook.com>
2022-05-01 14:16:11 -0400

    added Mobile Status!

--- js/index.js
+++ js/index.js
@@ -25,30 +25,47 @@ function gS() {
 	$.getJSON("https://api.lanyard.rest/v1/users/355295268716937227", data => {
 		data = data.data;
 		if (data.discord_status == "online") {
-			statusTag.classList.remove('dnd');
+      if (data.active_on_discord_mobile) {
+        statusTag.classList.remove('dnd');
+        statusTag.classList.remove('idle');
+        statusTag.classList.remove('offline');
+        statusTag.classList.remove('online');
+        statusTag.classList.remove('ping');
+        statusTag.classList.add('ping-mobile');
+        document.getElementById('text-status').innerHTML = "i am currently on mobile!"; 
+      } else {
+			statusTag.classList.remove('ping-mobile');
+      statusTag.classList.remove('dnd');
       statusTag.classList.remove('idle');
       statusTag.classList.remove('offline');
+      statusTag.classList.add('ping');
       statusTag.classList.add('online');
       document.getElementById('text-status').innerHTML = "i am currently online!"; 
-		}
+		}}
     if (data.discord_status == "offline") {
-			statusTag.classList.remove('online');
+			statusTag.classList.remove('ping-mobile');
+      statusTag.classList.remove('online');
       statusTag.classList.remove('idle');
       statusTag.classList.remove('dnd');
+      statusTag.classList.add('ping');
       statusTag.classList.add('offline');
       document.getElementById('text-status').innerHTML = "i am currently offline!";  
 		}
     if (data.discord_status == "dnd") {
-			statusTag.classList.remove('online');
+			statusTag.classList.remove('ping-mobile');
+      statusTag.classList.remove('online');
       statusTag.classList.remove('idle');
       statusTag.classList.remove('offline');
+      statusTag.classList.add('ping');
       statusTag.classList.add('dnd');
       document.getElementById('text-status').innerHTML = "i am currently on dnd!";  
 		}
     if (data.discord_status == "idle") {
-			statusTag.classList.remove('online');
+			statusTag.classList.remove('ping-mobile');
+      statusTag.classList.remove('online');
       statusTag.classList.remove('dnd');
       statusTag.classList.remove('offline');
+      statusTag.classList.add('ping');
       statusTag.classList.add('idle');
       document.getElementById('text-status').innerHTML = "i am currently away!";  
 		}
